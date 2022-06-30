e2fbeb5f7798b89c00a612021a0171d84a018860
cytern <2477185748@qq.com>
2022-06-30 12:56:47 +0800

    feature: 增加通用指令结果类

---
+++ src/main/java/com/cytern/pojo/CommandResult.java
@@ -0,0 +1,24 @@
+package com.cytern.pojo;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class CommandResult {
+    private Integer result;
+    private String returnMsg;
+
+    public Integer getResult() {
+        return result;
+    }
+
+    public void setResult(Integer result) {
+        this.result = result;
+    }
+
+    public String getReturnMsg() {
+        return returnMsg;
+    }
+
+    public void setReturnMsg(String returnMsg) {
+        this.returnMsg = returnMsg;
+    }
+}
