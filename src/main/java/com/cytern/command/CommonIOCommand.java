64d64e49d30e8b9e308f1e1cd4242105fadca960
cytern <2477185748@qq.com>
2022-07-22 20:16:16 +0800

    feature: 消息处理器异常处理

--- src/main/java/com/cytern/command/CommonIOCommand.java
+++ src/main/java/com/cytern/command/CommonIOCommand.java
@@ -3,7 +3,9 @@ package com.cytern.command;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.cytern.aspect.RobotCommand;
+import com.cytern.exception.RobotException;
 import com.cytern.service.impl.CommandExecutedService;
+import com.cytern.service.impl.LoggerService;
 import com.cytern.util.MessageSenderUtil;
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.message.data.MessageChain;
@@ -20,10 +22,30 @@ public class CommonIOCommand {
     @RobotCommand(activeService = "通用消息")
     public static JSONObject baseCommandHandler (JSONObject commands) {
         //基本消息服务应该分以下几步走
-        JSONArray preReturns = CommandExecutedService.preFilterReturn(commands);
-        JSONObject repeatFilterReturn = CommandExecutedService.repeatFilterReturn(preReturns,commands);
-        JSONObject newCommand = CommandExecutedService.preReturn(repeatFilterReturn, null);
-        MessageChain msg = CommandExecutedService.handleReturnMsg(newCommand);
+        JSONArray preReturns = null;
+        try {
+            preReturns = CommandExecutedService.preFilterReturn(commands);
+        } catch (RobotException e) {
+            LoggerService.error(e.getMessage());
+        }
+        JSONObject repeatFilterReturn = null;
+        try {
+            repeatFilterReturn = CommandExecutedService.repeatFilterReturn(preReturns,commands);
+        } catch (RobotException e) {
+            LoggerService.error(e.getMessage());
+        }
+        JSONObject newCommand = null;
+        try {
+            newCommand = CommandExecutedService.preReturn(repeatFilterReturn, null);
+        } catch (RobotException e) {
+            LoggerService.error(e.getMessage());
+        }
+        MessageChain msg = null;
+        try {
+            msg = CommandExecutedService.handleReturnMsg(newCommand);
+        } catch (RobotException e) {
+            LoggerService.error(e.getMessage());
+        }
         MessageSenderUtil.normalSend( (Contact) commands.get("subject"),msg);
         return null;
     }
