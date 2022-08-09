4338efca6ad84edb6027a54b177e35ce39841d8c
cytern <cytern@qq.com>
2022-08-09 18:07:01 +0800

    feature: 增加可视化爻语编辑器

---
+++ src/main/resources/mods/EditMods.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-row>
+    <el-col :span="24"><div class="grid-content ep-bg-purple-dark" /></el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="12"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="12"><div class="grid-content ep-bg-purple-light" /></el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="8"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="8"><div class="grid-content ep-bg-purple-light" /></el-col>
+    <el-col :span="8"><div class="grid-content ep-bg-purple" /></el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="6"><div class="grid-content ep-bg-purple-light" /></el-col>
+    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="6"><div class="grid-content ep-bg-purple-light" /></el-col>
+  </el-row>
+  <el-row>
+    <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+    <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+    <el-col :span="4"><div class="grid-content ep-bg-purple" /></el-col>
+    <el-col :span="4"><div class="grid-content ep-bg-purple-light" /></el-col>
+  </el-row>
+</template>
